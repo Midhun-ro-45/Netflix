629bbda3927a8d07181649899228ba0dcf2b6f04
srithargk23 <srithargk2307@gmail.com>
2023-12-04 13:01:54 +0530

    favorite list button

--- src/pages copy/portal/favourite/page.jsx
+++ src/pages copy/portal/favourite/page.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import { getWishlist } from "../../../api/getMovies";
 import MovieCard from "../../../components/movieCard/MovieCard";
 import "./page.css"
+import NavigationBar from "../../../components/navbar/Navbar";
+import Button from "../../../components/button/Button";
+import { Link } from "react-router-dom";
 
 const FavoriteListPage = () => {
     const [movies, SetMovies] = useState([])
@@ -9,7 +12,10 @@ const FavoriteListPage = () => {
         getWishlist().then((data) => SetMovies(data));
     }, []);
     return <div className="fav-container">
-        <h1 style={{ color: "white", fontWeight: "800" }}>My list:</h1>
+       
+       <div className="fav-home">
+        <div> <h1 style={{ color: "white", fontWeight: "800" }}>My List</h1> </div><div><Link to={"/home"}><Button width={"50px"} height={"35px"} content={"Home"} backgroundColor={"red"} color={"white"} padding={"4px"} borderRadius={"5px"} fontSize={"15px"}/></Link></div>
+</div>
         <div className="favourite-movie-list">{movies?.map((movie) => <MovieCard key={movie.id} movie={movie} />)}</div>
 
     </div>;
