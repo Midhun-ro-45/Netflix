1d464ec5641e12d47925990fba20605e3d64fdca
srithargk23 <srithargk2307@gmail.com>
2023-12-04 13:04:40 +0530

    row changed

--- src/components/row/Row.jsx
+++ src/components/row/Row.jsx
@@ -1,11 +1,12 @@
-import { List } from "./List";
+// import { List } from "./List";
 import Crasol from "../carosel/CustomCarosel";
 import MovieCard from "../movieCard/MovieCard";
+import "./row.css"
 
 const Row = ({ title, data }) => {
     return (
         <section>
-            {title && <h2>{title}</h2>}
+            {title && <h2 className="row-h2">{title}</h2>}
             {/* <Crasol data={data} /> */}
             <Crasol data={data} render={(item) => <MovieCard movie={item} />} />
         </section>
